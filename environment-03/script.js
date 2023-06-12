32ded2cacd6072ec4cd830e290bc1b58ac92c9a6
Nouhayla-code <124044952+Nouhayla-code@users.noreply.github.com>
2023-06-12 12:34:04 +0200

    .

--- environment-03/script.js
+++ environment-03/script.js
@@ -5,9 +5,3 @@
 // 1. Importer "data.js" i script-filen. Gem listen i en variabel.
 // 2. Vis alle events på hjemmesiden sorteret efter tidligste dato øverst.
 // 3. Filtrer, således kun events i sjette måned bliver vist på hjemmesiden.
-
-window.addEventListener("load",start);
-
-function start(){
-    
-}
--- environment-04/script.js
+++ environment-04/script.js
@@ -1,1 +1,25 @@
 "use strict";
+
+
+window.addEventListener("load", start)
+
+let tickets=[]; 
+
+function start(){
+    document.querySelector("#create-event-form").addEventListener("submit", addTickets)
+}
+
+function addTickets(event){
+    event.preventDefault(); 
+    const form=event.target;
+    const title=form.title.value; 
+    const description=form.description.value; 
+    const date=form.date.value; 
+
+    const ticket={title,description,date}
+
+    tickets.push(ticket)
+
+    console.log(tickets)
+
+}
